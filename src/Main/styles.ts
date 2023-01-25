6148241fd8a546a8cf23d721f1e49c856fef27b2
João Victor <jvfernandes.contato@gmail.com>
2023-01-25 16:52:30 -0300

    construction of homePage structure

--- src/Main/styles.ts
+++ src/Main/styles.ts
@@ -7,14 +7,24 @@ console.log({ isAndroid, currentHeight: StatusBar.currentHeight });
 
 export const Container = styled.SafeAreaView`
   margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
+  flex: 1;
+  background: #fafafa;
 `;
 
 export const CategoriesContainer = styled.View`
-
+  height: 73px;
+  margin-top: 34px;
 `;
+
 export const MenuContainer = styled.View`
-
+  flex: 1;
 `;
-export const FooterContainer = styled.View`
+
+export const Footer = styled.View`
+  min-height: 110px;
+  background: #fff;
+`;
+
+export const FooterContainer = styled.SafeAreaView`
 
 `;

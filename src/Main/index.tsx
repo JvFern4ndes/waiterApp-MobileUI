e8f282275351ab05fd390a81ebcff29b65e016d3
João Victor <jvfernandes.contato@gmail.com>
2023-01-25 15:59:02 -0300

    feat: handling SafeAreaView of systems statusBar dynamically

---
+++ src/Main/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from '../components/Text';
+
+import { Container } from './styles';
+
+export function Main() {
+  return (
+    <Container>
+      <Text>
+        Main renderizou
+      </Text>
+    </Container>
+  );
+}

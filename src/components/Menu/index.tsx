3b9fe5828cb7c374f77661f64fe4698a75171bc3
João Victor <jvfernandes.contato@gmail.com>
2023-01-26 20:53:25 -0300

    feat: menu component creation

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,17 @@
 import { FlatList } from 'react-native';
 
 import { products } from '../../mocks/products';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
 import { Text } from '../Text';
 
-import { Product, Image, ProductDetails } from './styles';
+import {
+  Product,
+  ProductImage,
+  ProductDetails,
+  Separator,
+  AddToCartButton,
+} from './styles';
 
 export function Menu() {
   return (
@@ -12,19 +20,26 @@ export function Menu() {
       style={{ marginTop: 32 }}
       contentContainerStyle={{ paddingHorizontal: 24 }}
       keyExtractor={product => product._id}
+      ItemSeparatorComponent={Separator}
       renderItem={({ item: product }) => (
         <Product>
-          <Image
+          <ProductImage
             source={{
               uri: `http://192.168.15.10:3001/uploads/${product.imagePath}`,
             }}
           />
 
           <ProductDetails>
-            <Text>{product.name}</Text>
-            <Text>{product.description}</Text>
-            <Text>{product.price}</Text>
+            <Text weight="600">{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight="600">{formatCurrency(product.price)}</Text>
           </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
         </Product>
       )}
     />

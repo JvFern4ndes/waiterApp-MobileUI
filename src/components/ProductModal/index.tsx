3ea2cb5855bbe19a23f9d4a6ed58ae30a2222829
João Victor <jvfernandes.contato@gmail.com>
2023-01-27 21:02:22 -0300

    feat: creation of ProductModal

---
+++ src/components/ProductModal/index.tsx
@@ -0,0 +1,72 @@
+import { FlatList, Modal } from 'react-native';
+import { Product } from '../../types/Product';
+import { Close } from '../Icons/Close';
+
+import { Text } from '../Text';
+import {
+  Image,
+  CloseButton,
+  ModalBody,
+  Header,
+  IngredientsContainer,
+  Ingredient,
+} from './styles';
+
+interface ProductModalProps {
+  visible: boolean;
+  onClose: () => void;
+  product: null | Product;
+}
+
+export function ProductModal({ visible, onClose, product }: ProductModalProps) {
+  if (!product) {
+    return null;
+  }
+
+  return (
+    <Modal
+      visible={visible}
+      animationType="slide"
+      presentationStyle="pageSheet"
+      onRequestClose={onClose}
+    >
+      <Image
+        source={{
+          uri: `http://192.168.15.10:3001/uploads/${product.imagePath}`,
+        }}
+      >
+        <CloseButton onPress={onClose}>
+          <Close />
+        </CloseButton>
+      </Image>
+
+      <ModalBody>
+        <Header>
+          <Text size={24} weight="600">{product.name}</Text>
+          <Text color="#666" style={{ marginTop: 8 }}>
+            {product.description}
+          </Text>
+        </Header>
+
+        <IngredientsContainer>
+          <Text weight="600" color="#666">Ingredientes</Text>
+
+          <FlatList
+            data={product.ingredients}
+            keyExtractor={ingredient => ingredient._id}
+            showsVerticalScrollIndicator={false}
+            style={{ marginTop: 16 }}
+            renderItem={({ item: ingredient }) => (
+              <Ingredient>
+                <Text>{ingredient.icon}</Text>
+                <Text size={14} color="#666" style={{ marginLeft: 20 }}>
+                  {ingredient.name}
+                </Text>
+              </Ingredient>
+            )}
+          />
+        </IngredientsContainer>
+      </ModalBody>
+    </Modal>
+  );
+}

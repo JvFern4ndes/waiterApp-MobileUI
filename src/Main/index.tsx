6a320fea2606c4cf3e6a520f05c26db7af23e057
João Victor <jvfernandes.contato@gmail.com>
2023-02-08 10:34:17 -0300

    feat: creating product listing through API consumption and adding new products

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,4 +1,5 @@
 import { ActivityIndicator } from 'react-native';
+import axios from 'axios';
 
 import {
   Container,
@@ -14,21 +15,32 @@ import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
 import { Button } from '../components/Button';
 import { TableModal } from '../components/TableModal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Cart } from '../components/Cart';
 import { CartItem } from '../types/CartItem';
 import { Product } from '../types/Product';
 
-import { products as mockProducts } from '../mocks/products';
 import { Empty } from '../components/Icons/Empty';
 import { Text } from '../components/Text';
+import { Category } from '../types/Category';
 
 export function Main() {
   const [isTableModalVisible, setIsTableModalVisible] = useState(false);
   const [selectedTable, setSelectedTable] = useState('');
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
   const [isLoading] = useState(false);
-  const [products] = useState<Product[]>(mockProducts);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    axios.get('http://192.168.15.10:3001/categories').then((response) => {
+      setCategories(response.data);
+    });
+
+    axios.get('http://192.168.15.10:3001/products').then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
 
   function handleSaveTable(table: string) {
     setSelectedTable(table);
@@ -109,7 +121,9 @@ export function Main() {
         {!isLoading && (
           <>
             <CategoriesContainer>
-              <Categories />
+              <Categories
+                categories={categories}
+              />
             </CategoriesContainer>
 
             {products.length > 0 ? (
